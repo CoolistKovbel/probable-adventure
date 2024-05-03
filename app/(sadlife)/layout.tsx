98f2ef5eadaf35b9f70ab22e70745a38bf151d27
Lyubomyr Kovbel <shpintz@Lyubomyrs-MacBook-Pro.local>
2024-05-03 15:56:32 +0300

    tt

--- app/(sadlife)/layout.tsx
+++ app/(sadlife)/layout.tsx
@@ -8,8 +8,8 @@ export default function SadLifeLayout({
     children: React.ReactNode;
   }>) {
     return (
-      <main className="min-h-screen w-full p-24 dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center flex-col">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <main className="min-h-screen w-full p-20 bg-black  bg-grid-black/[0.2] relative flex items-center justify-center flex-col">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <MainHeader />
         {children}
         <MainFooter />
--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full p-24  dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center flex-col">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+    <main className="min-h-screen w-full p-20  bg-black   bg-grid-black/[0.2] relative flex items-center justify-center flex-col">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <MainHeader />
 
       <section className="flex items-center justify-between flex-col mt-10 gap-5">
-        
         <header className="w-full md:w-[80%] bg-[#111] p-4 rounded-lg drop-shadow-lg flex items-center justify-around mx-auto md:flex-row flex-col gap-5">
           <div className="w-full md:w-[40%] text-center">
             <h2 className="text-2xl md:text-4xl font-bold mb-5 capitalize">
@@ -49,11 +48,9 @@ export default function Home() {
         <SimpleSwapPage />
 
         <Genereate />
-
       </section>
 
       <MainFooter />
-
     </main>
   );
 }
